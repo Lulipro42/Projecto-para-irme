5e319b7061bd93811107b14a93a05eb6aa1c21c2
Lulipro42 <solisulises422@gmail.com>
2026-06-01 16:39:13 -0300

    configurar whitenoise para los archivos css estaticos

--- software_engineer/curso_de_django/mysite/settings.py
+++ software_engineer/curso_de_django/mysite/settings.py
@@ -49,12 +49,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -121,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Esto le dice a Django dónde juntar todos los archivos CSS al compilar
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Esto habilita el almacenamiento comprimido de WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
